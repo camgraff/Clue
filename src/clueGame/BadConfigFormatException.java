112461ad2c72693fd6ef8393a15a4d41da9575a5
camerongraff <camerongraff@mines.edu>
2019-03-18 19:58:16 -0600

    Refactored badConfigFormatException to switch statement

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -4,20 +4,21 @@
 package clueGame;
 
 public class BadConfigFormatException extends Exception {
-	
+
 	public BadConfigFormatException() {
 		System.out.println("Error: There is a problem with the configuration files");
 		return;
 	}
-	
+
 	public BadConfigFormatException(String s) {
-		if (s == "badCols") {
+		switch (s) {
+		case "badCols" : 
 			System.out.println("Error: Rows do not all have the same number of columns");
 			return;
-		} else if (s == "badRoom") {
+		case "badRoom" :
 			System.out.println("Error: Room is not present in legend");
 			return;
-		} else if (s == "badFormat") {
+		case "badFormat" : 
 			System.out.println("Error: Room config file does not have correct format");
 			return;
 		}
