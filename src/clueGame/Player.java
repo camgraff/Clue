519285613e6c753cd8f734ed082e42f64732f2dc
James Mach <jamesmach@mines.edu>
2019-03-21 11:56:03 -0600

    Added rest of code to dealCards() in Board.java

--- src/clueGame/Player.java
+++ src/clueGame/Player.java
@@ -11,6 +11,7 @@ public class Player {
 	private ArrayList<Card> hand = new ArrayList<Card>();
 	
 	
+	
 	public Player(String playerName, int row, int column, Color color) {
 		super();
 		this.playerName = playerName;
@@ -67,5 +68,7 @@ public class Player {
 	public boolean isHuman() {
 		return false;
 	}
+	
+	
 		
 }
